35cbd52d37e22f9574de1ea64d3727fa3d6e09c1
Jacob Smith <jacob@jacobsmith.io>
2023-01-10 10:24:42 -0500

    Shave yaks

--- doom.d/config.el
+++ doom.d/config.el
@@ -29,79 +29,6 @@
 ;; `load-theme' function. This is the default:
 (setq doom-theme 'doom-one)
 
-(setq org-directory "~/Dropbox/org/")
-(setq org-archive-location "~/Dropbox/org/.archive/%s_archive::")
-(setq org-agenda-files '("~/Dropbox/org/things.org"
-                         "~/Dropbox/org/kajabi.org"
-                         "~/Dropbox/org-jira"))
-
-(setq org-default-notes-file "~/Dropbox/org/inbox.org")
-(after! org
-  (setq org-startup-folded t)
-  (setq org-log-done 'time)
-  (setq org-tag-alist '(("@work" . ?w) ("@home" . ?h) ("@computer" . ?c))))
-
-(setq org-todo-keywords
-      '((sequence "TODO(t)" "STARTED(s)" "WAITING(w)" "|" "DONE(d)" "CANCELLED(c) DEFERRED(f)")))
-
-(setq org-refile-targets '(("things.org" :maxlevel . 1)
-                           ("inbox.org" :level . 1)
-                           ("kajabi.org" :level . 1)
-                           ("kajabisomeday.org" :level . 2)
-                           ("someday.org" :level . 2)))
-
-
-;; Capture templates for: TODO tasks, Notes, appointments, phone calls, meetings, and org-protocol
-;; (setq org-capture-templates
-;;       (quote (("t" "todo" entry (file "~/git/org/refile.org")
-;;                "* TODO %?\n%U\n%a\n" :clock-in t :clock-resume t)
-;;               ("r" "respond" entry (file "~/git/org/refile.org")
-;;                "* NEXT Respond to %:from on %:subject\nSCHEDULED: %t\n%U\n%a\n" :clock-in t :clock-resume t :immediate-finish t)
-;;               ("n" "note" entry (file "~/git/org/refile.org")
-;;                "* %? :NOTE:\n%U\n%a\n" :clock-in t :clock-resume t)
-;;               ("j" "Journal" entry (file+datetree "~/git/org/diary.org")
-;;                "* %?\n%U\n" :clock-in t :clock-resume t)
-;;               ("w" "org-protocol" entry (file "~/git/org/refile.org")
-;;                "* TODO Review %c\n%U\n" :immediate-finish t)
-;;               ("m" "Meeting" entry (file "~/git/org/refile.org")
-;;                "* MEETING with %? :MEETING:\n%U" :clock-in t :clock-resume t)
-;;               ("p" "Phone call" entry (file "~/git/org/refile.org")
-;;                "* PHONE %? :PHONE:\n%U" :clock-in t :clock-resume t)
-;;               ("h" "Habit" entry (file "~/git/org/refile.org")
-;;                "* NEXT %?\n%U\n%a\nSCHEDULED: %(format-time-string \"%<<%Y-%m-%d %a .+1d/3d>>\")\n:PROPERTIES:\n:STYLE: habit\n:REPEAT_TO_STATE: NEXT\n:END:\n"))))
-
-(setq org-agenda-custom-commands
-      `(
-        ("P" "Plan The Day" ((agenda)
-                             (tags-todo "OFFICE")
-                             (tags-todo "HOME")
-                             (tags-todo "COMPUTER")))
-        ("T" "Today" ((agenda "" ((org-agenda-ndays 1)
-                                  (org-agenda-sorting-strategy
-                                   (quote ((agenda time-up priority-down tag-up) )))
-                                  (org-deadline-warning-days 0)
-                                  ))))
-        ))
-
-;; Remove empty LOGBOOK drawers on clock out
-(defun bh/remove-empty-drawer-on-clock-out ()
-  (interactive)
-  (save-excursion
-    (beginning-of-line 0)
-    (org-remove-empty-drawer-at "LOGBOOK" (point))))
-
-(add-hook 'org-clock-out-hook 'bh/remove-empty-drawer-on-clock-out 'append)
-
-(setq org-jira-working-dir "~/Dropbox/org-jira")
-(setq jiralib-url "https://kajabi.atlassian.net")
-(setq org-roam-directory "~/Dropbox/org/")
-
-;; This determines the style of line numbers in effect. If set to `nil', line
-;; numbers are disabled. For relative line numbers, set this to `relative'.
-(setq display-line-numbers-type t)
-
-(setq menu-bar-mode t)
-
 ;; Here are some additional functions/macros that could help you configure Doom:
 ;;
 ;; - `load!' for loading external *.el files relative to this one
@@ -119,9 +46,64 @@
 ;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
 ;; they are implemented.
 
-(setq org-capture-templates
-      (quote (("t" "todo" entry (file "~/Dropbox/org/todo.org")
-               "* TODO %?\n%a\n"))))
+(after! org
+  (setq org-agenda-custom-commands `(("P" "Plan The Day" ((agenda)
+                                                          (tags-todo "OFFICE")
+                                                          (tags-todo "HOME")
+                                                          (tags-todo "COMPUTER")))
+                                     ("T" "Today" ((agenda "" ((org-agenda-ndays 1)
+                                                               (org-agenda-sorting-strategy
+                                                                '((agenda time-up priority-down tag-up) ))
+                                                               (org-deadline-warning-days 0)
+                                                               ))))))
+  (setq org-agenda-files '("~/Dropbox/org/next.org"
+                           "~/Dropbox/org/projects.org"
+                           "~/Dropbox/org-jira"))
+  (setq org-archive-location "~/Dropbox/org/.archive/%s_archive::")
+  (setq org-capture-templates
+        '(("t" "todo" entry (file "~/Dropbox/org/inbox.org")
+           "* TODO %?\n%U\n%a\n" :clock-in t :clock-resume t)
+          ("r" "respond" entry (file "~/Dropbox/org/inbox.org")
+           "* TODO Respond to %:from on %:subject\nSCHEDULED: %t\n%U\n%a\n" :clock-in t :clock-resume t :immediate-finish t)
+          ("n" "note" entry (file "~/Dropbox/org/inbox.org")
+           "* %? :NOTE:\n%U\n%a\n" :clock-in t :clock-resume t)
+          ("w" "org-protocol" entry (file "~/Dropbox/org/inbox.org")
+           "* TODO Review %c\n%U\n" :immediate-finish t)
+          ("p" "Phone call" entry (file "~/Dropbox/org/inbox.org")
+           "* TODO %? :PHONE:\n%U" :clock-in t :clock-resume t)))
+  (setq org-default-notes-file "~/Dropbox/org/inbox.org")
+  (setq org-directory "~/Dropbox/org/")
+  (setq org-log-done 'time)
+  (setq org-refile-targets '(("~/Dropbox/org/inbox.org" :level . 0)
+                             ("~/Dropbox/org/next.org" :level . 0)
+                             ("~/Dropbox/org/projects.org" :maxlevel . 1)
+                             ("~/Dropbox/org/someday.org" :maxlevel . 2)))
+  (setq org-startup-folded t)
+  (setq org-tag-alist '(("@work" . ?w) ("@home" . ?h) ("@computer" . ?c)))
+  (setq org-todo-keywords
+        '((sequence "TODO(t)" "STARTED(s)" "WAITING(w)" "|" "DONE(d)" "CANCELLED(c) DEFERRED(f)"))))
+
+(after! org-jira
+  (setq jiralib-update-issue-fields-exclude-list '(priority components))
+  (setq jiralib-url "https://kajabi.atlassian.net")
+  (setq org-jira-custom-jqls
+        '(
+          (:jql "status IN ('Backlog', 'To Do', 'In Progress', 'Work in progress', 'Open', 'Specify') AND assignee = 'Jake Smith'"
+           :limit 50
+           :filename "my-work")
+          ))
+  (setq org-jira-working-dir "~/Dropbox/org-jira"))
+
+(after! org-roam
+  (setq +org-roam-open-buffer-on-find-file nil)
+  (setq org-roam-link-auto-replace nil)
+  (setq org-roam-directory "~/Dropbox/org-roam/"))
+
+;; This determines the style of line numbers in effect. If set to `nil', line
+;; numbers are disabled. For relative line numbers, set this to `relative'.
+(setq display-line-numbers-type t)
+
+(setq menu-bar-mode t)
 
 (defun systemist/copy-as-rtf ()
   "Export region to RTF and copy it to the clipboard."
@@ -148,15 +130,11 @@
 
 (add-hook 'dired-mode-hook 'dired-hide-details-mode)
 
-(setq +org-roam-open-buffer-on-find-file nil)
-
 (map! :nv "gx" #'browse-url-at-point)
 
 (map! :n "M-RET" #'toggle-frame-fullscreen)
 
 (map! :n "-" #'dired-jump)
-
-(setq org-roam-link-auto-replace nil)
 
 (defun systemist/resize-org-images (&rest _)
   (setq org-image-actual-width (list (round (* 0.8 (window-body-width nil t)))))
@@ -171,20 +149,6 @@
     'org-babel-load-languages
     '((mermaid . t)
       (elisp . t)))
-
-;; (defconst org-jira-progress-issue-flow
-;;   '(
-;;     ("To Do" . "In Progress"
-;;     ("In Progress" . "Done"))))
-
-(setq org-jira-custom-jqls
-  '(
-    (:jql "status IN ('Backlog', 'To Do', 'In Progress', 'Work in progress', 'Open', 'Specify') AND assignee = 'Jake Smith'"
-     :limit 50
-     :filename "my-work")
-    ))
-
-(setq jiralib-update-issue-fields-exclude-list '(priority components))
 
 (setq format-all-formatters
       '(("SQL" pgformatter)))
