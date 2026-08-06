28478d8bf1fc0555860b490131a3907a5d47ae15
Jacob Smith <jacob@jacobsmith.io>
2026-08-05 21:53:51 -0400

    Shave yaks

--- config/doom/config.el
+++ config/doom/config.el
@@ -22,8 +22,8 @@
 ;; font string. You generally only need these two:
 ;; (setq doom-font (font-spec :family "Operator Mono SSm for Powerline" :size 16)
 ;;       doom-variable-pitch-font (font-spec :family "sans" :size 13))
-(setq doom-font (font-spec :family "OperatorMonoSSm Nerd Font Mono" :size 16)
-      doom-variable-pitch-font (font-spec :family "Recursive Sans Casual Medium" :size 16))
+(setq doom-font (font-spec :family "MonoLisaCode" :size 16 :weight medium)
+      doom-variable-pitch-font (font-spec :family "MonoLisaText" :size 16 :weight medium))
 
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
--- zshenv
+++ zshenv
@@ -3,3 +3,11 @@ export PATH="$HOME/.cargo/bin:$PATH"
 [[ -f ~/.zshenv.local ]] && source ~/.zshenv.local
 
 export DO_NOT_TRACK=1
+
+if [[ "$(arch)" == "arm64" ]]; then
+  brew_path="/opt/homebrew/bin/brew"
+else
+  brew_path="/usr/local/bin/brew"
+fi
+
+eval "$("$brew_path" shellenv)"
